bc7e05b423288d2868e3d0250b83812fc2f6325c
semilore <semilore@omotade-michaels.com>
2023-05-31 16:23:18 +0100

    add: basic game structure

--- rps/app.js
+++ rps/app.js
@@ -1,63 +1,42 @@
-const choices = ["rock", "paper", "scissors"];
+"use strict;";
 
-const getComputerChoice = () => {
-  let choice = Math.floor(Math.random() * 3);
-  let ComputerChoice = choices[choice];
-  return ComputerChoice;
-  //   console.log(ComputerChoice);
+const computerInput = () => {
+  const options = ["rock", "paper", "scissors"];
+  const random = Math.round(Math.random() * 2);
+
+  return options[random];
 };
 
-// getComputerChoice();
+// console.log(computerInput());
 
-userChoice = prompt("rock, paper or scissors? ");
-
-// const cpu = getComputerChoice();
-const play = (user, cpu) => {
-  user = user.toLowerCase();
-  //   console.log(cpu);
-  if (user == cpu) {
-    return "Its a draw";
-  } else if (user == "rock" && cpu == "scissors") {
-    return "User wins: 1 point";
-  } else if (user == "scissors" && cpu == "rock") {
-    return "Cpu wins: 1 point";
-  } else if (user == "rock" && cpu == "paper") {
-    return "Cpu wins: 1 point";
-  } else if (user == "paper" && cpu == "rock") {
-    return "User wins: 1 point";
-  } else if (user == "paper" && cpu == "scissors") {
-    return "Cpu wins: 1 point";
-  } else if (user == "scissors" && cpu == "paper") {
-    return "User wins: 1 point";
+const round = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    return "oops... it was a draw. Play again!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return `oops... Computer wins ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return `oops... Computer wins ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return `oops... Computer wins ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `Hurray! Player wins ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return `Hurray! Player wins ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return `Hurray! Player wins ${playerSelection} beats ${computerSelection}`;
   }
 };
 
-// play(userChoice, getComputerChoice);
-// console.log(play(userChoice));
+// console.log(round(playerSelection, computerSelection));
 
-const game = (rounds) => {
-  let userScore = 0;
-  let cpuScore = 0;
-  let draws = 0;
-  play(userChoice, getComputerChoice());
-  for (let i = 0; i < rounds; i++) {
-    if (play(userChoice, getComputerChoice()) == "User wins: 1 point") {
-      userScore++;
-    } else if (play(userChoice, getComputerChoice()) == "Its a draw") {
-      draws++;
-    } else {
-      cpuScore++;
-    }
+const game = () => {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("choose rock, paper or scissors... ");
+    const computerSelection = computerInput();
+    console.log(round(playerSelection, computerSelection));
+    // round();
   }
-
-  if (userScore > cpuScore) {
-    console.log(`User wins: by ${userScore - cpuScore} points`);
-  } else {
-    console.log(`Cpu wins: by ${cpuScore - userScore} points`);
-  }
-
-  console.log(`User: ${userScore}`);
-  console.log(`Cpu: ${cpuScore}`);
 };
 
-game(3);
+game();
